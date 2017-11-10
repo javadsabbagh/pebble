e2a960b621b60096c0f93f88ca47e82ffa5daf9f
javad <j.sabbagh89@gmail.com>
2017-11-10 22:09:29 +0330

    project structure based on gradle

---
+++ src/main/java/com/foss/pebble/Application.java
@@ -0,0 +1,10 @@
+package com.foss.pebble;
+
+import javafx.stage.Stage;
+
+public class Application extends javafx.application.Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}
